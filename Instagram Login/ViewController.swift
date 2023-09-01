014b4c2ed917acc64b60434ae0f6917a656e0672
Kerem Demir <keremdemir1694@gmail.com>
2023-09-01 15:21:27 +0300

    Programmatic design set.

---
+++ Instagram Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram Login
+//
+//  Created by Kerem Demir on 1.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
